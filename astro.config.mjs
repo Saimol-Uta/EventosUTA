3b9a1a71f8e8dd2ad7ceb9bdf2c1d8d3e3ae14e7
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-05-14 08:51:35 -0500

    Se creo el Proyecto Astro y se añadio la dependencia que se van a usar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import clerk from "@clerk/astro";
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [clerk()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
+
+    adapter: netlify(),
+    output: 'server',
+});
