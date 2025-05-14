58afb9c181834d2e535a377df1afe4aeb3a12cd3
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-05-14 18:33:07 -0500

    Se elimino la referencia a clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-import clerk from "@clerk/astro";
+
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [clerk()],
+    integrations: [],
     vite: {
         plugins: [tailwindcss()]
     },
