26b32c536361bc8cd135b1fed4a52e25972eba7a
Brayan Pilla <bpilla9393@uta.edu.ec>
2025-05-23 11:37:11 -0500

    Se agrego el archivo de configuración de authjs para las credenciales de usuarios

---
+++ auth.config.ts
@@ -0,0 +1,72 @@
+import { defineConfig } from 'auth-astro';
+import Google from '@auth/core/providers/google';
+import Credentials from '@auth/core/providers/credentials';
+import prisma from './src/db';
+import type { AdapterUser } from '@auth/core/adapters';
+import bcrypt from 'bcryptjs';
+
+interface CustomUser {
+    id: string;
+    email: string;
+    rol: string;
+    ci_pas: string
+}
+
+export default defineConfig({
+    providers: [
+        Credentials({
+            credentials: {
+                email: { label: 'Correo', type: 'email' },
+                password: { label: 'Contraseña', type: 'password' },
+            },
+            authorize: async (credentials) => {
+                try {
+                    // 1. Validar que las credenciales no estén vacías
+                    if (!credentials?.email || typeof credentials.password !== 'string') { // Asegurar que password también es string
+                        console.log('Faltan credenciales o el formato es incorrecto.');
+                        return null;
+                    }
+
+                    const user = await prisma.cuentas.findFirst({
+                        where: {
+                            cor_cue: credentials.email
+                        }
+                    });
+
+                    if (!user || typeof user.cont_cuenta !== 'string') {
+                        if (!user) {
+                            console.log('Usuario no encontrado:', credentials.email);
+                        } else {
+                            // Esto indica que el usuario existe pero cont_cuenta es null o no es un string.
+                            // Para un login con credenciales, esto podría ser un estado anómalo.
+                            console.error('El usuario:', credentials.email, 'no tiene un hash de contraseña válido.');
+                        }
+                        return null;
+                    }
+
+                    // Devolver el usuario con la misma estructura que Google
+                    return {
+                        id: user.id_cue,
+                        email: user.cor_cue,
+                        rol: user.rol_cue,
+                        ci_pas: user.enl_ced_cue
+                    } as CustomUser;
+
+                } catch (error) {
+                    console.error('Error en autenticación:', error);
+                    return null;
+                }
+            }
+        }),
+    ],
+    // Páginas de inicio de sesión y error
+    pages: {
+        signIn: '/login',
+        error: '/login',
+    },
+
+    callbacks: {
+
+   
+    }
+});
