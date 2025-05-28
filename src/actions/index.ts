38cbb4b87cff54a86b1e86670f0af0e945fb61f3
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-05-28 16:46:03 -0500

    se unio la logica con el frontend

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,11 @@
 import { getEventoBySlug, getEventos } from './Eventos';
 import { SignIn } from './auth/registerUser.action';
 import { getEventosPorUsuario } from './Eventos/getEventosPorUsuario'
+import { createCambio } from './Cambios';
 export const server = {
     getEventos,
     getEventoBySlug,
     SignIn,
-    getEventosPorUsuario
+    getEventosPorUsuario,
+    createCambio,
 };
