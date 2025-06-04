e275911a417f77b771201afec179a5b02409fd04
carol104 <carol.28427@gmail.com>
2025-06-04 18:37:05 -0500

    se arregló la funcionalidad de obtener los eventos por usuario para el historial de eventos

--- src/actions/Eventos/getEventosPorUsuario.ts
+++ src/actions/Eventos/getEventosPorUsuario.ts
@@ -6,19 +6,27 @@ export const getEventosPorUsuario = defineAction({
   accept: 'json',
 
   input: z.object({
-    idUsuario: z.string().uuid(), 
+    idUsuario: z.string().uuid(),
   }),
 
   handler: async ({ idUsuario }) => {
     try {
+      
       const inscripciones = await prisma.inscripciones.findMany({
         where: { id_usu_ins: idUsuario },
-        include: {
-          eventos: true,
-        },
+        select: { id_eve_ins: true },
       });
 
-      const eventos = inscripciones.map(i => i.eventos);
+      const idsEventos = inscripciones.map(i => i.id_eve_ins);
+
+      // Buscar eventos con esos IDs
+      const eventos = await prisma.eventos.findMany({
+        where: {
+          id_eve: {
+            in: idsEventos,
+          },
+        },
+      });
 
       return {
         success: true,
