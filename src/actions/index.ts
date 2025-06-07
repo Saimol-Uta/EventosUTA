cd093527283dc64abf0a4537e1f689cf709e22cf
Marlon Guevara <141768047+MarlonKuna26@users.noreply.github.com>
2025-06-07 12:28:33 -0500

    En este commit se imito el estilo deseado, para en base a este tener los campos necesarios

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -24,7 +24,7 @@ export const server = {
 
     getCertificadosPorUsuario,
     getEventosProximos,
-    getInscripcionesPendientes
+    getInscripcionesPendientes,
 
     getOrganizadorByEvento,
     getOrganizadores,
