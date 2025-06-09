221cab05b691fad69f901210fc0ce733c74c53ce
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-08 23:26:19 -0500

    se importo todos los metodos

--- src/actions/Users/index.ts
+++ src/actions/Users/index.ts
@@ -4,3 +4,5 @@ export * from './getUsers';
 export * from './setUsers.action';
 export * from './uploadDocumentImage.action';
 export * from './uploadImgeUser.action';
+export * from './eliminar-usuario.action';
+export * from './modificar-usuario.action';
