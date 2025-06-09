78144ba3ff79d49d5c944f05841682b3b6d82d7b
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-09 01:04:24 -0500

    Se modifico la action para tener todas las cuentas asociadas al usuario y se carga la informacion en el modal que esta dentro del otro modal

--- src/actions/Users/getCuentaById.action.ts
+++ src/actions/Users/getCuentaById.action.ts
@@ -7,7 +7,7 @@ export const getCuentaById = defineAction({
     input: z.string(),
     handler: async (id_cuenta) => {
         // Acción
-        const cuenta = await prisma.cuentas.findFirst({
+        const cuenta = await prisma.cuentas.findMany({
             where: { id_usu_per: id_cuenta },
             include: {
                 usuarios: true
