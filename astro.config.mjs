bfefacb69b900aeda1b98c7513562e450423c7e7
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-24 18:43:16 -0500

    Configuracion completa con Docker para desplegar

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import auth from 'auth-astro'; // <-- Reactívalo
 import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [auth()], // <-- Reactívalo
+    integrations: [auth()],
+    vite: {
+        plugins: [tailwindcss()]
+    },
     output: 'server',
     adapter: node({
         mode: 'standalone'
