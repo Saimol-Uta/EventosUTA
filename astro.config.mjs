f57ff4d3a433c8653b925c231ab6438cac9f5d63
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-24 09:55:36 -0500

    ArchivosDOkerCreado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
-
 import auth from 'auth-astro';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [auth()],
-    vite: {
-        plugins: [tailwindcss()]
-    },
-    output: 'server',
-    adapter: netlify(),
+  integrations: [auth()],
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
