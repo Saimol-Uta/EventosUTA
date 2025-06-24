087bd8e2f93c61649ef19cdc2ab77e67df4658f4
carol104 <carol.28427@gmail.com>
2025-06-24 16:57:56 -0500

    EU-33 Se corrigió la lógica para editar los datos de la página principal

--- src/actions/Admin/updatePaginaPrincipal.ts
+++ src/actions/Admin/updatePaginaPrincipal.ts
@@ -1,47 +1,49 @@
 import { defineAction } from 'astro:actions';
-import { z } from 'astro:schema';
+import { string, z } from 'astro:schema';
 import prisma from '../../db';
 
 export const updatePaginaPrincipal = defineAction({
   accept: 'form',
   input: z.object({
-    des_pag: z.string().optional(),
-    mis_pag: z.string().optional(),
-    vis_pag: z.string().optional(),
+    nom_fac: z.string().optional(),
+    des_fac: z.string().optional(),
+    mis_fac: z.string().optional(),
+    vis_fac: z.string().optional(),
 
     // Nuevos campos para la tarjeta del decano
     dec_fac: z.string().optional(),
     sub_fac: z.string().optional(),
-    sub_fac2: z.string().optional(),
+    dec_img: z.string().optional(),
     sub_img: z.string().optional(), // Base64 o URL
   }),
-  handler: async ({ des_pag, mis_pag, vis_pag, dec_fac, sub_fac, sub_fac2, sub_img }) => {
+  handler: async ({ nom_fac, des_fac, mis_fac, vis_fac, dec_fac, sub_fac, dec_img, sub_img }) => {
     try {
       const updateData: any = {};
-
-      if (des_pag !== undefined) updateData.des_pag = des_pag;
-      if (mis_pag !== undefined) updateData.mis_pag = mis_pag;
-      if (vis_pag !== undefined) updateData.vis_pag = vis_pag;
+       if (nom_fac !== undefined) updateData.des_pag = nom_fac;
+      if (des_fac !== undefined) updateData.des_pag = des_fac;
+      if (mis_fac !== undefined) updateData.mis_pag = mis_fac;
+      if (vis_fac !== undefined) updateData.vis_pag = vis_fac;
 
       if (dec_fac !== undefined) updateData.dec_fac = dec_fac;
       if (sub_fac !== undefined) updateData.sub_fac = sub_fac;
-      if (sub_fac2 !== undefined) updateData.sub_fac2 = sub_fac2;
+      if (dec_img !== undefined) updateData.sub_fac2 = dec_img;
       if (sub_img !== undefined) updateData.sub_img = sub_img;
 
-      const paginaPrincipal = await prisma.pagina_principal.upsert({
+      const paginaPrincipal = await prisma.facultades.upsert({
         where: {
-          id_pag: 1
+          id_fac: 'a1b2c3d4-e5f6-4a7b-8c9d-1a1a1a1a1a1a'
         },
         update: updateData,
         create: {
-          id_pag: 1,
-          des_pag: des_pag || '',
-          mis_pag: mis_pag || '',
-          vis_pag: vis_pag || '',
+          id_fac: 'a1b2c3d4-e5f6-4a7b-8c9d-1a1a1a1a1a1a',
+          nom_fac: nom_fac || '',
+          des_fac: des_fac || '',
+          mis_fac: mis_fac || '',
+          vis_fac: vis_fac || '',
 
           dec_fac: dec_fac || '',
+          dec_img: dec_img || '',
           sub_fac: sub_fac || '',
-          sub_fac2: sub_fac2 || '',
           sub_img: sub_img || '',
         }
       });
