bf1b431739b4dcd38560dee8879c7ee5098d0545
Saimol-Uta <sjimenez6271@uta.edu.ec>
2025-06-24 00:13:05 -0500

    uso de node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,23 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import netlify from '@astrojs/netlify';
+
 
 import auth from 'auth-astro';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [auth()],
+
     vite: {
         plugins: [tailwindcss()]
     },
+
     output: 'server',
-    adapter: netlify(),
+
+    adapter: node({
+        mode: 'standalone',
+    }),
 });
